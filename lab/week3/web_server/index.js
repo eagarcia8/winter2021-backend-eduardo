2216abc87b0e42b31589856a9ac698468f4a2e8e
Eduardo <medatech@medasf.org>
2021-11-10 11:52:52 -0800

    Worked on HTTP POST Request

--- lab/week3/web_server/index.js
+++ lab/week3/web_server/index.js
@@ -1,13 +1,20 @@
 // Use the Express package.
 const express = require("express");
 
+// Use the body-parser package.
+const bodyParser = require("body-parser");
+
 // Creates an Express object from the Express package.
 const app = express();
 
 // Allows Express to use the HTTP module of Node.
 const http = require("http").Server(app);
 
-const port = 80;
+// Necessary settings for body-parser, which automatically detects and converts JSON in our HTTP requests.
+app.use(express.json({strict: false}));
+app.use(express.urlencoded({extended: true}));
+
+const port = 3000;
 
 // Tells Express/HTTP what port to listen to.
 http.listen(port); // Common Development Ports: 8080, 3000 = Node.
@@ -16,3 +23,16 @@ console.log("Express server is running on port " + port + ".");
 
 // Express Routes. Allows us to "redirect" a URL filepath to a specific folder.
 app.use("/", express.static("public_html/"));
+
+// This .post() method handles any POST requests made by the /test filepath. When a request is made, run the callback function.
+app.post("/test", function (request, response) {
+    // Data from the front end is located inside of request.body.
+
+    // The following code runs when a request is made.
+    console.log("The front end sends the following: ", request.body);
+
+    // We send a response to the front-end client with a Status Code of 200 (which means OK).
+    response.sendStatus(200);
+});
+
+
