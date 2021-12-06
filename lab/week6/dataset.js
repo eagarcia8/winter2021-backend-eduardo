bc53f8ad3a8192b033cf86802bd44bbb583c95f8
Eduardo <medatech@medasf.org>
2021-12-06 11:28:48 -0800

    Finished dataset lab project

--- lab/week6/dataset.js
+++ lab/week6/dataset.js
@@ -104,7 +104,7 @@ for (let site in treeSite) {
 
     // console.log(siteArray);
 
-    // Check if the first array element holds the word Sidewalk.
+    // Check if the first array element holds the word Sidewalk. Does not check for space character after "Sidewalk".
     if (siteArray[0] === "Sidewalk") {
         sidewalkTally.sidewalk += treeSite[site];
     } else {
@@ -121,7 +121,91 @@ let answerQ1 = `The percentage of sidewalk trees vs other trees in San Francisco
 console.log(answerQ1);
 
 // Whats the most frequent Tree Species?
+console.log(treeObjects[38928]);
+
+let treeSpecies = {};
+
+for (let i = 0; i <  treeObjects.length; i++) {
+
+    let currentTree = treeObjects[i];
+
+    // Check if the treeSite object has a property that is the same as the current qSpecies VALUE.
+    if (treeSpecies.hasOwnProperty(currentTree.qSpecies)) {
+        treeSpecies[currentTree.qSpecies] += 1;
+    } else {
+        treeSpecies[currentTree.qSpecies] = 1;
+    }
+}
+
+// console.log(treeSpecies);
+
+let highestSpecies = "";
+let highestCount = 0;
+
+delete treeSpecies["Tree(s) ::"];
+
+for (let species in treeSpecies) {
+    let currentSpeciesCount = treeSpecies[species];
+
+    if (highestCount < currentSpeciesCount) {
+        highestSpecies = species;
+        highestCount = currentSpeciesCount
+    }
+}
+
+console.log(`The most frequent tree species for streets in San Francisco is ${highestSpecies}.`);
 
 // From trees that have a plant date, how many were planted in 1955?
 
+let dates = {};
+let noDateCount = 0;
+
+console.log(treeObjects[2]);
+
+for (let i = 0; i < treeObjects.length; i++) {
+
+    let date = treeObjects[i].PlantDate;
+
+    if (date === "") {
+        noDateCount += 1;
+        continue;
+    }
+
+    let dateArray = date.split(" ");
+
+    // EXAMPLE: dateArray = ["2002", "Apr", "01", "12:00:00", "AM"]
+
+    let monthdayyear = dateArray[0];
+    let yearArray = monthdayyear.split("/");
+    let year = yearArray[2];
+
+    if (dates.hasOwnProperty(year)) {
+        dates[year] += 1;
+    } else {
+        dates[year] = 1;
+    }
+}
+
+console.log(`There were ${dates["1955"]} trees planted in 1955.`);
+
+
 // How many trees does DPW take care of?
+
+let careTaker = {};
+
+for (let i = 0; i < treeObjects.length; i++) {
+
+    let currentTree = treeObjects[i];
+
+    let owner = currentTree.qCaretaker;
+
+    if (careTaker.hasOwnProperty(owner)) {
+        careTaker[owner] += 1;
+    } else {
+        careTaker[owner] = 1;
+    }
+}
+
+console.log(careTaker);
+
+console.log(`DPW takes care of ${careTaker["DPW"]}.`);
