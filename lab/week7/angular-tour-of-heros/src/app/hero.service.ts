190c4ee6020cdbaf81a0df5632a6560a3bb69d7d
Eduardo <medatech@medasf.org>
2021-12-14 11:56:48 -0800

    Finished part 5 of Angular Tutorial

--- lab/week7/angular-tour-of-heros/src/app/hero.service.ts
+++ lab/week7/angular-tour-of-heros/src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 
 @Injectable({
@@ -9,10 +10,20 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+
+    this.messageService.add('HeroService: fetched heroes');
+
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+
+    return of(hero);
   }
 }
